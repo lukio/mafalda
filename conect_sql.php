8a0f5830ae8340846b494b3dba0d226a7cefc69d
Luciano Rossi <lucho@luchorossi.com.ar>
2008-03-20 20:03:20 +0000

    agregando MDB2 para mysql

---
+++ conect_sql.php
@@ -0,0 +1,60 @@
+<?php
+require_once ('include/pear/MDB2.php');
+
+$dsn = array(
+            'phptype'  => 'mssql',
+            'username' => 'administrator',
+            'password' => 'vamosmujer',
+            'hostspec' => '10.1.1.2',
+            'database' => 'flexar',
+        );
+$dsnmysql = array(
+            'phptype'  => 'mysql',
+            'username' => 'root',
+            'password' => 'agitatormiike',
+            'hostspec' => '127.0.0.1',
+            'database' => 'usla',
+        );
+
+$options = array(
+            'debug'       => 2,
+            'portability' => MDB2_PORTABILITY_ALL,
+        );
+
+/*$mdb2 = mssql_connect("10.1.1.2", "administrator","vamosmujer");
+mssql_select_db("flexar", $mdb2)
+    or die ("Can't connect to Database");
+
+    $resultado = mssql_query('select modelo from modelos');
+
+    echo $resultado;
+*/
+
+//uses MDB2::factory() to create the instance
+ // and also attempts to connect to the host
+
+  
+$mdb2 =& MDB2::connect($dsnmysql, $options);
+if (PEAR::isError($mdb2)) {
+     die($mdb2->getMessage());
+}
+
+$res =& $mdb2->query('select * from usla_session');
+
+echo $res->numCols();
+
+/*while (($row = $res->fetchrow())) {
+       // Assuming MDB2's default fetchmode is MDB2_FETCHMODE_ORDERED
+     echo $row[0] . "\n";
+     }
+
+if (PEAR::isError($res)) {
+        die($res->getMessage());
+}
+*/
+$mdb2->disconnect();
+
+
+?>
+
+
